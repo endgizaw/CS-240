ee9bc707c394e1e3e39c2d48c5209ccc18cd46b9
endgizaw <end.gizaw@gmail.com>
2026-09-06 21:55:31 -0700

    Add files via upload

--- Assignment 1/hw1.py
+++ Assignment 1/hw1.py
@@ -1,15 +1,16 @@
 from PIL import Image
 
-# Build an ASCII-to-decimal converter.
+# 1. Build an ASCII-to-decimal converter.
 
 # s = "Endalk"
 # for c in s:
 #    print(ord(c))
 
 
-# Build a number-base converter supporting binary, decimal, octal, and hexadecimal.
+# 2. Build a number-base converter supporting binary, decimal, octal, and hexadecimal.
 
-# Write a program that reads an image and prints its pixel values.
+
+# 3. Write a program that reads an image and prints its pixel values.
 
 # def convert(pixel):
 #     if pixel == "(237, 28, 36)":
@@ -36,16 +37,16 @@ from PIL import Image
 
 # output_text_file.close()
 
-# Write a program that consumes pixel values and creates an image.
+# 4. Write a program that consumes pixel values and creates an image.
 
 
 def convert(pixel):
-    if pixel == (237, 28, 36):
-        return "R"
-    elif pixel == (0, 0, 0):
-        return "B"
-    elif pixel == (255, 242, 0):
-        return "Y"
+    if pixel == "R":
+        return (237, 28, 36)
+    elif pixel == "B":
+        return (0, 0, 0)
+    elif pixel == "Y":
+        return (255, 242, 0)
     else:
         return pixel
 
@@ -53,14 +54,12 @@ def convert(pixel):
 input_text_file = open("awesome_picture.txt", "r")
 lines = input_text_file.readlines()
 h, w = len(lines), lines[0].count(" ")
-img = Image.new(mode="RGB", size=(h, w), color=(0, 0, 0))
+img = Image.new(mode="RGB", size=(w, h), color=(0, 0, 0))
 
 for y in range(h):
     pixels = lines[y].split()
-    print(pixels)
     for x in range(w):
         pixel = pixels[x]
-        img.putpixel((x, y), convert(pixels))
-    print()
+        img.putpixel((x, y), convert(pixel))
 
-# Test boundary cases, including zero, the largest supported unsigned value, and at least one negative two's-complement value.
+# 5. Test boundary cases, including zero, the largest supported unsigned value, and at least one negative two's-complement value.
